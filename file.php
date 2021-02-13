be6c4ba17c74610cd7018bb3cf0b6e8f62f08297
Denny Lee <dennylee5595@gmail.com>
2021-02-14 05:39:50 +0700

    file php tambahan 5

--- file.php
+++ file.php
@@ -1,9 +1,8 @@
 <?php
-echo "hello world";
 $link = mysqli_connect("172.18.0.2","root","bebas","Trucorp");
 $query = "SELECT * from Trucorp.users";
 $res = mysqli_query($link, $query);
-$count = mysqli_nuw_rows($res);
+$count = mysqli_num_rows($res);
 echo "Jumlah user ada " . $count;
 mysqli_close($link);
 ?>
