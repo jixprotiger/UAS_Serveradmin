6d26f685162d8320417cc441840bc72ee5337fa2
Denny Lee <dennylee5595@gmail.com>
2021-02-14 05:31:47 +0700

    file php tambahan 3

--- file.php
+++ file.php
@@ -1,5 +1,5 @@
 <?php
-echo "hellooo world";
+echo "hello world";
 $link = mysqli_connect("172.18.0.2","root","bebas","Trucorp");
 $query = "SELECT * from Trucorp.users";
 $res = mysqli_query($link, $query);
